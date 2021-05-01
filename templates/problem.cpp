51707099cf6659d555ff950bdc64f711dcaae6d5
Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>
2021-05-01 11:57:51 +0200

    templates: update `problem.cpp`
    Signed-off-by: Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>

--- templates/problem.cpp
+++ templates/problem.cpp
@@ -1,5 +1,82 @@
-#include <cstdint>
 #include <iostream>
+#include <limits>
+
+namespace me
+{
+    template <class _Integer>
+    class IntegerWrapper
+    {
+        static_assert(std::is_integral_v<_Integer>);
+    public:
+        constexpr IntegerWrapper() noexcept = default;
+
+        constexpr IntegerWrapper(const _Integer __i) noexcept
+            : m_integer_(__i)
+        {}
+
+        constexpr operator _Integer() const noexcept { return m_integer_; }
+    private:
+        _Integer m_integer_;
+    };
+
+    using int8_t = IntegerWrapper<std::int8_t>;
+    using uint8_t = IntegerWrapper<std::uint8_t>;
+
+    template <class _CharT, class _Traits, class _Integer>
+    std::basic_ostream<_CharT, _Traits>&
+    operator<<(std::basic_ostream<_CharT, _Traits>& __o, IntegerWrapper<_Integer> __n)
+    {
+        switch (__o.flags() & std::ios::basefield) {
+            case std::ios_base::hex:
+            case std::ios_base::oct:
+                return (__o << static_cast<long>(static_cast<std::make_unsigned_t<_Integer>>(__n)));
+            default:
+                return (__o << static_cast<long>(__n));
+        }
+    }
+
+    template <class _CharT, class _Traits, class _Integer>
+    std::basic_istream<_CharT, _Traits>&
+    operator>>(std::basic_istream<_CharT, _Traits>& __i, IntegerWrapper<_Integer>& __n)
+    {
+        using num_get = std::num_get<_CharT, std::istreambuf_iterator<_CharT, _Traits>>;
+        using sentry = typename std::basic_istream<_CharT, _Traits>::sentry;
+
+        sentry s(__i);
+
+        if (s) {
+            auto err = std::ios_base::goodbit;
+
+            try {
+                long l;
+                std::use_facet<num_get>(__i.getloc()).get(__i, 0, __i, err, l);
+
+                if (l < std::numeric_limits<_Integer>::min()) {
+                    err |= std::ios_base::failbit;
+                    __n = std::numeric_limits<_Integer>::min();
+                } else if (l > std::numeric_limits<_Integer>::max()) {
+                    err |= std::ios_base::failbit;
+                    __n = std::numeric_limits<_Integer>::max();
+                } else {
+                    __n = static_cast<_Integer>(l);
+                }
+#ifdef __GLIBCXX__
+            } catch (__cxxabiv1::__forced_unwind&) {
+                __i.setstate(std::ios_base::badbit);
+                throw;
+#endif
+            } catch (...) {
+                __i.setstate(std::ios_base::badbit);
+            }
+
+            if (err) {
+                __i.setstate(err);
+            }
+        }
+
+        return __i;
+    }
+}
 
 int main()
 {
